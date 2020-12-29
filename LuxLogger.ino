18e6b519d2202caebe3260de961e5fc2350b1459
OverTheRhine <64443462+OverTheRhine@users.noreply.github.com>
2020-12-29 21:40:51 +0100

    Update LuxLogger.ino
    Some minor things to help the code better readable.

--- LuxLogger.ino
+++ LuxLogger.ino
@@ -10,37 +10,37 @@
  *  Resources used: 22190 Bytes (77%) of Program Memory. Maximum are 28672 Bytes.
  *  Courtesy: Adafruit https://github.com/adafruit/Adafruit_TSL2561/blob/master/examples/sensorapi/sensorapi.ino 
  *  and https://learn.adafruit.com/adafruit-feather-32u4-adalogger/using-the-sd-card?embeds=allow
-*/
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
+ *  This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+ *  which provides a common 'type' for sensor data and some helper functions.
    
-   To use this driver you will also need to download the Adafruit_Sensor library and include
-   it in your libraries folder.
+ *  To use this driver you will also need to download the Adafruit_Sensor library and include
+ *  it in your libraries folder.
 
-   You should also assign a unique ID to this sensor for use with the Adafruit Sensor API
-   so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply provide an appropriate value
-   in the constructor below (12345 is used by default in this example).
+ *  You should also assign a unique ID to this sensor for use with the Adafruit Sensor API
+ *  so that you can identify this particular
+ *  sensor in any data logs, etc.  To assign a unique ID, simply provide an appropriate value
+ *  in the constructor below (12345 is used by default in this example).
    
-   Connections
-   ===========
-   Connect SCL to I2C SCL Clock
-   Connect SDA to I2C SDA Data
-   Connect VCC/VDD to 3.3V or 5V (depends on sensor's logic level, check the datasheet)
-   Connect GROUND to common ground
+ * Connections
+ * ===========
+ *  Connect SCL to I2C SCL Clock
+ *  Connect SDA to I2C SDA Data
+ *  Connect VCC/VDD to 3.3V or 5V (depends on sensor's logic level, check the datasheet)
+ *  Connect GROUND to common ground
 
-   I2C Address
-   ===========
-   The address will be different depending on whether you leave
-   the ADDR pin floating (addr 0x39), or tie it to ground or vcc. 
-   The default addess is 0x39, which assumes the ADDR pin is floating
-   (not connected to anything).  If you set the ADDR pin high
-   or low, use TSL2561_ADDR_HIGH (0x49) or TSL2561_ADDR_LOW
-   (0x29) respectively.
+ *  I2C Address
+ *  ===========
+ *  The address will be different depending on whether you leave
+ *  the ADDR pin floating (addr 0x39), or tie it to ground or vcc. 
+ *  The default addess is 0x39, which assumes the ADDR pin is floating
+ *  (not connected to anything).  If you set the ADDR pin high
+ *  or low, use TSL2561_ADDR_HIGH (0x49) or TSL2561_ADDR_LOW
+ *  (0x29) respectively.
     
-   History
-   =======
-   2013/JAN/31  - First version (KTOWN)
+ *  History
+ *  =======
+ *  2013/JAN/31  - First version (KTOWN)
+ *  2020/DEC/14 Something new from OverTheRhine@backhaus-net.de
 */
 #include <SPI.h>
 #include <SD.h>
@@ -70,7 +70,7 @@ Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 1234
     error(4): "Error opening: " (logfile)
 */
 /**************************************************************************/
-void error(uint8_t errno){
+void error(uint8_t errno) {
   while(1) {
     uint8_t i;
     for (i=0; i<errno; i++){ // blink requested code
